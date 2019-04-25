88c7c6c1cc7b81b1e4b2d967e3a1b23c40173acd
hritvik12 <49869591+hritvik12@users.noreply.github.com>
2019-04-25 21:28:04 +0530

    Add files via upload

---
+++ dnssfinal.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo " Welcome to dns configration script"
+
+yum install bind* -y
+
+wget -A.conf 'https://raw.githubusercontent.com/hritvik12/hritviksc/master/named.conf'
+
+cp named.conf /etc/
+
+wget -A.conf 'https://raw.githubusercontent.com/hritvik12/manualsh/master/forward1' -P /var/named/
+
+echo "Input your Ip address sir"
+
+read ip_address
+  
+echo www	IN	A	 $ip_address >> /var/named/forward1
+
+
+
+chgrp named	/var/named/forward1
+
+systemctl restart named
+
+firewall-cmd --permanent --add-port=53/tcp
+
+firewall-cmd --permanent --add-port=53/udp
+
+firewall-cmd --reload
+
+rm /etc/hosts
+
+rm /etc/resolv.conf
+
+nslookup www.google.com
+echo "ban gyi script"
+
