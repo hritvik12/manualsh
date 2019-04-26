6ab2908c64ff647876a21583aab9a884b2658339
hritvik12 <49869591+hritvik12@users.noreply.github.com>
2019-04-27 02:53:31 +0530

    Add files via upload

---
+++ manualnew.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "Hello World"
+echo "1. yum 2.dnsfinal 3.apache 4.samba 5.useradd"
+read CHOICE
+
+
+case "$CHOICE" in
+"1")
+wget -A.conf 'https://raw.githubusercontent.com/hritvik12/manualsh/master/yummy.sh' -P/
+. yummy.sh;;
+
+"2")
+wget -A.conf 'https://raw.githubusercontent.com/hritvik12/hritviksc/master/dnssfinal.sh' -P /
+. dnssfinal.sh;;
+
+"3")
+wget -A.conf 'https://raw.githubusercontent.com/hritvik12/manualsh/master/apache180.sh' -P /
+. apache180.sh;;
+"4")
+wget -A.conf 'https://raw.githubusercontent.com/hritvik12/manualsh/master/sambas.sh' -P /
+. sambas.sh;;
+"5")
+wget -A.conf 'https://raw.githubusercontent.com/hritvik12/manualsh/master/useradd.sh' -P /
+. useradd.sh;;
+
+
+
+esac
+
